c928f2fdfcbc4f55741ccc4ffee1246a27ed6dba
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2022-10-10 13:05:26 +0200

    implemented reproduction

---
+++ BirthdateClosedXML/Program.cs
@@ -0,0 +1,11 @@
+﻿using ClosedXML.Excel;
+using System.Reflection;
+
+using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"BirthdateClosedXML.Persons.xlsx"))
+using (var workbook = new XLWorkbook(stream)) // --> throws FormatException
+{
+    var worksheet = workbook.Worksheets.Worksheet("Aktionäre");
+    Console.WriteLine($"Firstname: {worksheet.Cell("A2").GetValue<string>()}");
+    Console.WriteLine($"Lastname: {worksheet.Cell("B2").GetValue<string>()}");
+    Console.WriteLine($"Birthdate: {worksheet.Cell("C2").GetValue<DateTime?>()}");
+}
